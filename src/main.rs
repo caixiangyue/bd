cbcd02235a6fc5772b49ce9bd081ce8259a9e47a
caixiangyue <caixiangyue007@gmail.com>
2022-01-30 12:55:39 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use reqwest::header::{REFERER, USER_AGENT};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
     if args.len() < 2 {
         exit(-1);
     }
@@ -20,74 +19,58 @@ fn main() {
                 exit(-1);
             }
     }
-    c_url.push_str(bvid);
-    let cid: serde_json::Value;
-    let res = get_cid(&c_url);
-    match res {
-        Ok(v)=> {
-            cid = v;
-        }
-        Err(e) => {
-            eprintln!("get bvid error: {}", e);
-            exit(-1);
-        }
+
+    download(&mut c_url, &mut g_url, bvid);
+}
+
+fn get_bvid(url: &str) -> Option<&str> {
+    match url.find("BV") {
+        Some(bv_index) => Some(&url[bv_index..bv_index+12]),
+        None => None
     }
-    let cid = cid.as_u64().unwrap().to_string();
-    println!("{}", cid);
+}
+
+#[tokio::main]
+async fn download(c_url: &mut String, g_url: &mut String, bvid: &str) -> Result<(), Box<dyn std::error::Error>> {
+    c_url.push_str(bvid);
+    let r1 = reqwest::get(c_url.as_str())
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
+    let title = &r1["data"]["title"];
+    let v= &r1["data"]["cid"];
+    let cid = v.as_u64().unwrap().to_string();
 
     g_url.push_str("?bvid=");
     g_url.push_str(bvid);
     g_url.push_str("&cid=");
     g_url.push_str(cid.as_str());
     g_url.push_str("&qn=80");
-
-    println!("{}", g_url);
-    get_real(&g_url, bvid);
-}
-
-fn get_bvid(url: &str) -> Option<&str> {
-    match url.find("BV") {
-        Some(bv_index) => Some(&url[bv_index..]),
-        None => None
-    }
-}
-
-#[tokio::main]
-async fn get_cid(c_url: &String) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
-    let resp = reqwest::get(c_url)
-        .await?
-        .json::<serde_json::Value>()
-        .await?;
-    let cid= resp["data"]["cid"].clone();
-    // Box::new(cid);
-    // println!("{}", cid);
-    Ok(cid)
-}
-
-#[tokio::main]
-async fn get_real(g_url: &String, bvid: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get(g_url)
+    
+    
+    let r2 = reqwest::get(g_url.as_str())
         .await?
         .json::<serde_json::Value>()
         .await?;
 
-    let v = &resp["data"]["durl"][0]["backup_url"][0];
+    let v = &r2["data"]["durl"][0]["url"];
     let real_url = v.as_str().unwrap();
-    println!("{}", real_url);
+    println!("正在下载 {}，请稍后。。。", title.as_str().unwrap());
     let client = reqwest::Client::builder()
         .build()?;
-    let resp = client.get(real_url)
+    let r3 = client.get(real_url)
             .header(REFERER, "https://www.bilibili.com")
             .header(USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0")
             .send()
             .await?
             .bytes()
             .await?;
-    
-    let mut fname = String::from(bvid);
-    fname.push_str(".flv");
-    let mut file = fs::File::create(fname).unwrap();
-    file.write_all(&resp).unwrap();
 
+    let mut fname = String::from(title.as_str().unwrap());
+    fname.push_str(".flv");
+    let mut file = fs::File::create(&fname)?;
+    file.write_all(&r3)?;
+
+    println!("{} 下载完成", fname);
     Ok(())
 }
