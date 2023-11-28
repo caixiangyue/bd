38e1cf4d0b4c01f15b0937f37b88bc0129703221
caixiangyue <caixiangyue007@gmail.com>
2023-11-28 14:24:11 +0800

    improve code

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,103 @@
-use std::{process::exit, env, fs, io::Write};
-use reqwest::header::{REFERER, USER_AGENT};
 use env_logger::Env;
+use reqwest::blocking::Client;
+use reqwest::header::{REFERER, USER_AGENT};
+use serde_json::{from_str, Value};
+use std::{env, fs, io::Write, process::exit};
+
+#[derive(Debug)]
+struct Bilibili {
+    cli: Client,
+    cid_url: String,
+    cid: String,
+    play_url: String,
+    url: String,
+    real_url: String,
+    bvid: String,
+    title: String,
+}
+
+impl Bilibili {
+    fn new(url: String, play_url: String, mut cid_base_url: String) -> Self {
+        let mut bvid = "".to_string();
+        if let Some(bv_index) = url.find("BV") {
+            if url.len() >= bv_index + 12 {
+                bvid.push_str(&url[bv_index..bv_index + 12]);
+                cid_base_url.push_str(&url[bv_index..bv_index + 12]);
+            }
+        }
+        Self {
+            cli: Client::new(),
+            cid: "".to_string(),
+            cid_url: cid_base_url,
+            play_url: play_url,
+            url: url,
+            real_url: "".to_string(),
+            bvid: bvid,
+            title: "".to_string(),
+        }
+    }
+
+    fn get_cid(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let r = self.cli.get(&self.cid_url).send()?;
+        let json: Value = from_str(&r.text()?).unwrap();
+        let title = json["data"]["title"].as_str().unwrap();
+        let cid = json["data"]["cid"].as_u64().unwrap().to_string();
+        self.title = title.to_string();
+        self.cid = cid;
+        Ok(())
+    }
+
+    fn get_real_url(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        self.play_url.push_str("?bvid=");
+        self.play_url.push_str(&self.bvid);
+        self.play_url.push_str("&cid=");
+        self.play_url.push_str(&self.cid);
+        self.play_url.push_str("&qn=120");
+        let r = self.cli.get(&self.play_url).send()?;
+        let json: Value = from_str(&r.text()?).unwrap();
+        self.real_url = json["data"]["durl"][0]["url"].as_str().unwrap().to_string();
+        Ok(())
+    }
+
+    fn download(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let _ = self.get_cid();
+        let _ = self.get_real_url();
+        info!("正在下载 {}，请稍后。。。", self.title);
+        let r = self.cli.get(&self.real_url)
+            .header(REFERER, "https://www.bilibili.com")
+            .header(USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0")
+            .send()?
+            .bytes()?;
+        let mut fname = self.title.clone();
+        fname.push_str(".flv");
+        let mut file = fs::File::create(&fname)?;
+        file.write_all(&r)?;
+
+        info!("{} 下载完成", fname);
+
+        Ok(())
+    }
+}
 
 #[macro_use]
 extern crate log;
 
 fn main() {
     logger_init();
-
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         error!("argument is error");
         exit(-1);
     }
 
-    if let Some(bvid) = get_bvid(&args[1]) {
-        download(bvid);
-    } else {
-        error!("url is invalid");
-    }
+    let mut bili = Bilibili::new(
+        args[1].clone(),
+        String::from("https://api.bilibili.com/x/player/playurl"),
+        String::from("https://api.bilibili.com/x/web-interface/view?bvid="),
+    );
+    let _ = bili.download();
 }
 
 fn logger_init() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-}
-
-fn get_bvid(url: &str) -> Option<&str> {
-    if let Some(bv_index) = url.find("BV") {
-        if url.len() < bv_index+12 {
-            return None
-        } else {
-            return Some(&url[bv_index..bv_index+12])
-        }
-    }
-    None
-}
-
-#[tokio::main]
-async fn download(bvid: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let mut c_url = String::from("https://api.bilibili.com/x/web-interface/view?bvid=");
-    let mut g_url = String::from("https://api.bilibili.com/x/player/playurl");
-
-    c_url.push_str(bvid);
-    let r1 = reqwest::get(c_url.as_str())
-        .await?
-        .json::<serde_json::Value>()
-        .await?;
-    let title = &r1["data"]["title"];
-    let v= &r1["data"]["cid"];
-    let cid = v.as_u64().unwrap().to_string();
-
-    g_url.push_str("?bvid=");
-    g_url.push_str(bvid);
-    g_url.push_str("&cid=");
-    g_url.push_str(cid.as_str());
-    g_url.push_str("&qn=80");
-    
-    
-    let r2 = reqwest::get(g_url.as_str())
-        .await?
-        .json::<serde_json::Value>()
-        .await?;
-
-    let v = &r2["data"]["durl"][0]["url"];
-    let real_url = v.as_str().unwrap();
-    info!("正在下载 {}，请稍后。。。", title.as_str().unwrap());
-    let client = reqwest::Client::builder()
-        .build()?;
-    let r3 = client.get(real_url)
-            .header(REFERER, "https://www.bilibili.com")
-            .header(USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0")
-            .send()
-            .await?
-            .bytes()
-            .await?;
-
-    let mut fname = String::from(title.as_str().unwrap());
-    fname.push_str(".flv");
-    let mut file = fs::File::create(&fname)?;
-    file.write_all(&r3)?;
-
-    info!("{} 下载完成", fname);
-    Ok(())
 }
